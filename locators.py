03cce4b0f8ce6e2ef9fb7658cd719a6079adf281
astha62 <astha2211029@iimscollege.edu.np>
2025-08-25 21:35:00 +0545

    Added locators for SauceDemo login page

---
+++ locators.py
@@ -0,0 +1,16 @@
+#Username input box:
+XPath: //input[@id='user-name']
+CSS: #user-name
+
+#Password input box:
+XPath: //input[@id='password']
+CSS: #password
+
+#Login button:
+XPath: //input[@id='login-button']
+CSS: #login-button
+
+#Error message:
+XPath: //h3[@data-test='error']
+CSS: h3[data-test='error']
+
